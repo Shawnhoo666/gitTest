8c516279a833cd145c8fcd5d1c0ff617130809b9
cwtch666 <zhuhao@newdt.cn>
2019-03-14 19:54:31 +0800

    commit from cwtch6666

---
+++ src/main/java/cn/newdt/GitTest/Cwtch666.java
@@ -0,0 +1,20 @@
+package cn.newdt.GitTest;
+
+public class Cwtch666 {
+	/*
+	 * asdfadfadf
+	 * adfa
+	 * dfadf
+	 * adfa
+	 * dfadf
+	 * adsff
+	 * asdf
+	 * 
+	 * adfadfadf
+	 * adfa
+	 * dfads
+	 * adf
+	 * adfadf
+	 * adfadfadf
+	 */
+}
